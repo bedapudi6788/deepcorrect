ad1fcc627778ee0343850109dfd85e17d7fe64ba
Bedapudi Praneeth <bedapudi@Bedapudis-MacBook-Air.local>
2019-02-01 17:13:34 +0530

    using beam size

--- deepcorrect/deepcorrect.py
+++ deepcorrect/deepcorrect.py
@@ -8,11 +8,11 @@ class DeepCorrect():
         DeepCorrect.deepcorrect_model = build_model(params_path)
         DeepCorrect.deepcorrect_model[0].load_weights(checkpoint_path)
     
-    def correct(self, sentence):
+    def correct(self, sentence, beam_size = 1):
         if not DeepCorrect.deepcorrect_model:
             print('Please load the model first')
 
         sentence = sentence.strip()
-        sentence = infer(sentence, DeepCorrect.deepcorrect_model[0], DeepCorrect.deepcorrect_model[1])
+        sentence = infer(sentence, DeepCorrect.deepcorrect_model[0], DeepCorrect.deepcorrect_model[1], beam_size = beam_size)
         
         return sentence
